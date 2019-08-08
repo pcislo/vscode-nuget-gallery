e53896d2cea6988ecf918550d0edfdfbd87cee14
Patryk Cislo <patrykcislo@gmail.com>
2019-08-08 22:37:29 +0200

    add simple view example

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
+let fs = require("fs");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-nuget-gallery" is now active!');
+	context.subscriptions.push(vscode.commands.registerCommand('extension.start', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'nuget-gallery', // Identifies the type of the webview. Used internally
+			'NuGet Gallery', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		var html = fs.readFileSync(path.join(context.extensionPath, 'web', 'index.html'), "utf8");
+		panel.webview.html = html;
+	}));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
