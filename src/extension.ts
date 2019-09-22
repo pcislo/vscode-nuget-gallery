d2e0de58e2296c0270eabf5d9d2d3b3b7d87038e
Patryk Cislo <patrykcislo@gmail.com>
2019-09-22 18:12:12 +0200

    add  task

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,33 @@ const fs = require("fs");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+function loadProjects(panel: vscode.WebviewPanel) {
+	vscode.workspace.findFiles("**/*.csproj").then(files => {
+		let projects = Array();
+		files.map(x => x.fsPath).forEach(x => {
+			let document = new dom().parseFromString(fs.readFileSync(x, "utf8"));
+			let packagesReferences = xpath.select("//ItemGroup/PackageReference", document);
+			let project = {
+				path: x,
+				project: path.basename(x),
+				packages: Array()
+			};
+			packagesReferences.forEach((p: any) => {
+				let projectPackage = {
+
+					id: p.attributes.getNamedItem("Include").value,
+					version: p.attributes.getNamedItem("Version").value
+				};
+				project.packages.push(projectPackage);
+			});
+			projects.push(project);
+		});
+
+		panel.webview.postMessage(projects.sort((a, b) => a.path < b.path ? -1 : a.path > b.path ? 1 : 0));
+	});
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.start', () => {
@@ -19,44 +46,51 @@ export function activate(context: vscode.ExtensionContext) {
 			{ enableScripts: true } // Webview options. More on these later.
 		);
 
-		panel.webview.onDidReceiveMessage(
-			message => {
-				console.log(message);
+		let queue: vscode.Task[] = [];
 
-				switch (message.command) {
-					case 'install':
-						return;
-					case 'uninstall':
-						return;
+		vscode.tasks.onDidEndTask(async e => {
+			let next = queue.shift();
+			if (next !== undefined) {
+				await vscode.tasks.executeTask(next);
+			}
+			else {
+				loadProjects(panel);
+			}
+		});
+
+		panel.webview.onDidReceiveMessage(
+			async message => {
+				if (message.command === "reloadProjects") {
+					loadProjects(panel);
+				}
+				else {
+					for (let i = 0; i < message.projects.length; i++) {
+						let project = message.projects[i];
+						let args = [message.command, project.path, "package", message.package.id];
+						if (message.command === 'add') {
+							args.push("-v");
+							args.push(message.version);
+						}
+						let task = new vscode.Task(
+							{ type: 'dotnet', task: `dotnet ${message.command}` },
+							'nuget-gallery',
+							'dotnet',
+							new vscode.ShellExecution("dotnet", args)
+						);
+						queue.push(task);
+					}
+					let next = queue.shift();
+					if (next !== undefined) {
+						await vscode.tasks.executeTask(next);
+					}
+
 				}
 			},
 			undefined,
 			context.subscriptions
 		);
 
-		vscode.workspace.findFiles("**/*.csproj").then(files => {
-			let projects = Array();
-			files.map(x => x.fsPath).forEach(x => {
-				let document = new dom().parseFromString(fs.readFileSync(x, "utf8"));
-				let packagesReferences = xpath.select("//ItemGroup/PackageReference", document);
-				let project = {
-					path: x,
-					project: path.basename(x),
-					packages: Array()
-				};
-				packagesReferences.forEach((p: any) => {
-					let projectPackage = {
 
-						id: p.attributes.getNamedItem("Include").value,
-						version: p.attributes.getNamedItem("Version").value
-					};
-					project.packages.push(projectPackage);
-				});
-				projects.push(project);
-			});
-
-			panel.webview.postMessage(projects.sort((a, b) => a.path < b.path ? -1 : a.path > b.path ? 1 : 0));
-		});
 
 		let html = fs.readFileSync(path.join(context.extensionPath, 'web', 'index.html'), "utf8");
 		panel.webview.html = html;
